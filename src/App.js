c7c09bf4913b52528def24ca334dcc3030c3c970
austinbhan <austinbhan@outlook.com>
2022-06-09 12:56:24 -0700

    I dont know what i'm doing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import VehicleList from './VehicleList';
 import TrafficLight from './TrafficLight';
+import useState from 'react';
 
 function App() {
   // track the following state with a few useState hooks:
